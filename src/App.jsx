6f820202624c89e41a836d7f23b1fd5e272679b4
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-09-15 22:52:42 +0700

    materialize setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // init Materialize JS
+    M.AutoInit();
+  });
+
   return (
     <div>
       <h1>Hello world</h1>
