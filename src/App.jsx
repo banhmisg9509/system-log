bf233dcdd00611cec999fe49f0227f94258a35d4
banhmisg9509 <banhmisg9509@users.noreply.github.com>
2020-09-24 22:40:26 +0700

    redux store provider setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import {
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import React, { Fragment, useEffect } from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './store';
 
 function App() {
   useEffect(() => {
@@ -19,17 +21,19 @@ function App() {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 }
 
